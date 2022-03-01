d0ac8436c3cde88b17b50528380a3bc2ea898a33
khalilnazari <khalil.deve@gmail.com>
2022-03-01 23:31:30 +0800

    Update the Responsiveness of home page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-
+// Navbar hide and show
+const navbar = document.querySelector('.navbar'); 
 document.querySelector('#navbar_toggle_btn').addEventListener('click', () => {
-    document.querySelector('.navbar').classList.toggle('show'); 
+    navbar.classList.add('show'); 
 })
+
+document.querySelector('#close_navbar_btn').addEventListener('click', () => {
+    navbar.classList.remove('show'); 
+})
+
+
 
 // Set title 
 const url = document.URL; 
 if(url.includes('pages/shop')) {
-    document.title = "Home Page - ecommerce"; 
-    console.log('home')
+    document.title = "Shop - ecommerce"; 
 }
+
+
